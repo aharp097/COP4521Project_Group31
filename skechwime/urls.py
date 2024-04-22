84510a06de2b83134f862e8de7edd39b31ff830b
aharp097 <114882973+aharp097@users.noreply.github.com>
2024-04-21 23:32:22 -0400

    Update urls.py

--- skechwime/urls.py
+++ skechwime/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
-from views.canvas_view import canvas_view
+"""skechwime URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
-    path('canvas/', canvas_view, name='canvas'),  # This pattern expects that canvas_view is a view function or class
-    path('', canvas_view, name='home'),  # This line handles the root URL
+    path('admin/', admin.site.urls),
+    path('canvas/', include(('canvas.urls', 'canvas'), namespace='canvas')),
+    path('lobby/', include(('canvas.urls', 'lobby'), namespace='lobby')),
+    path('__debug__', include(debug_toolbar.urls))
 ]
