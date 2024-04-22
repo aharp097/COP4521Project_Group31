058d2a5d1716e93b0ef5e6820e3a6d99ca785d2c
aharp097 <114882973+aharp097@users.noreply.github.com>
2024-04-22 19:43:48 -0400

    Update models.py

--- canvas/models.py
+++ canvas/models.py
@@ -7,12 +7,12 @@ class Canvas(models.Model):
     name = models.CharField(max_length=255)
     layers = models.JSONField(default=list)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='canvases')
-
+    lobby = models.OneToOneField('Lobby', on_delete=models.CASCADE, related_name='uniquecanvas', null=True, blank=True)
     def __str__(self):
         return self.name
 
 class Lobby(models.Model):
-    canvas = models.OneToOneField(Canvas, on_delete=models.CASCADE, null=True, blank=True, default='')
+    canvas = models.OneToOneField(Canvas, on_delete=models.CASCADE, related_name='uniquelobby', null=True, blank=True, default='')
     creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default='')
     name = models.CharField(max_length=255, unique=True)
     users = models.ManyToManyField(User, related_name='lobbies')
