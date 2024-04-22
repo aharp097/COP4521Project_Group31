ea77685b8cd5e7ae96d2ec731dfa932fd016785d
aharp097 <114882973+aharp097@users.noreply.github.com>
2024-04-22 19:03:13 -0400

    Update urls.py

--- canvas/urls.py
+++ canvas/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lobby/', views.lobby_view, name='lobby'),
+    path('lobby/<int:lobby_id>/', views.lobby_view, name='lobby'),
     path('', views.home_view, name='home'),
     path('create_lobby/', views.create_lobby, name='create_lobby'),
     path('join_lobby/', views.join_lobby, name='join_lobby'),
     path('canvas/<int:lobby_id>/', views.canvas_view, name='canvas'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login_view'),
+    path('signup/', views.signup_view, name='signup_view'),
     path('logout/', views.logout_view, name='logout')
 ]
