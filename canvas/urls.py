da16023f5e9fa5b47161e6b91a0981b1353f8ee2
aharp097 <114882973+aharp097@users.noreply.github.com>
2024-04-22 20:44:33 -0400

    Delete canvas directory

--- canvas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.canvas_view, name='canvas'),
-    path('lobby/', views.lobby_view, name='lobby'),
-    path('', views.canvas_view, name='home'),
-]
