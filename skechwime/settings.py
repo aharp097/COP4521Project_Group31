7553f70c56f03ac6db6214121c5ef5c31ccfa1ae
John Negrin <108360449+JohnNegrin@users.noreply.github.com>
2024-04-30 16:15:36 -0400

    Update settings.py

--- skechwime/settings.py
+++ skechwime/settings.py
@@ -44,7 +44,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(REDIS_URL,)],
+            "hosts": [REDIS_URL],
         },
     },
 }
